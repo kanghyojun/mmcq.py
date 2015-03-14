236d36b5351250fc2db087caf8cd989f405540ba
Kang Hyojun <hyojun@admire.kr>
2015-03-14 15:44:22 +0900

    Use `find_packages`

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #! -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from mmcq.version import VERSION
 
@@ -7,7 +7,7 @@ setup(name='mmcq.py',
       version=VERSION,
       author='Kang Hyojun',
       author_email='hyojun@admire.kr',
-      packages=['mmcq'],
+      packages=find_packages(),
       install_requires=[
           'Wand >= 0.3.0'
       ])
